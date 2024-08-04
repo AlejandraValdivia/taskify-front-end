14569dc22388a8a7399163bc9e66b2f696af2c3b
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-03 19:40:05 -0700

    feat: add tskify-back-end-render link to vite.config

--- src/components/SigninForm/SigninForm.jsx
+++ src/components/SigninForm/SigninForm.jsx
@@ -10,7 +10,7 @@ const initialState = {
 };
 
 const SigninForm = (props) => {
-  const navigate = useNavigate(); // added this for navigation purposes
+  const navigate = useNavigate(); 
   const [message, setMessage] = useState([""]);
   const [formData, setFormData] = useState({
     username: "",
@@ -39,46 +39,60 @@ const SigninForm = (props) => {
   };
 
   return (
-    <Container>
-      <h1>Login</h1>
-      <p>{message}</p>
-      <Form noValidate autoComplete="off" onSubmit={handleSubmit}>
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a valid username address.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a valid password.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button variant="success" type="submit">
-          Log In
-        </Button>
-        <Button variant="danger" type="submit">
-          Cancel
-        </Button>
-      </Form>
-    </Container>
+    <div className="signup-page container">
+      <Container className="signup-container">
+        <Row>
+          <Col>
+            <h1 className="text-center my-5 text-black">
+              Signup to get started
+            </h1>
+            <p>{message}</p>
+          </Col>
+        </Row>
+        <Form
+          noValidate
+          className="signup-form"
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <Form.Group className="mb-3" controlId="formUsername">
+            <Form.Label>Username:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid username address.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a valid password.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="success" type="submit">
+            Log In
+          </Button>
+          <br />
+          <Button className="red-btn" variant="danger" type="submit">
+            Cancel
+          </Button>
+        </Form>
+      </Container>
+    </div>
   );
 };
 
